b6d35cd8537d2504beb51aeaedf96053ab9d6e0d
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2017-12-25 20:39:53 +0800

    添加获取模块

---
+++ actions/getModules.js
@@ -0,0 +1,15 @@
+const testFolder = '/home/mi/workspace/miui-sys-front/src/modules';
+const fs = require('fs');
+
+
+
+module.exports = function rewriteServer(req, res, next) {
+    fs.readdir(testFolder, (err, files) => {
+        console.log(err);
+        if(err) {
+            res.send({status: false})
+            return;
+        }
+     	res.send({status: true, data: files})
+    })
+}
