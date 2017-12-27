9e2d42139333fcbb1cd2468f291a1c6266c36509
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2017-12-27 17:32:00 +0800

    添加tooltip

--- conf/ips.js
+++ conf/ips.js
@@ -1,19 +1,4 @@
-let name = {
-    zhangyang: 'http://10.232.39.9:8082',
-    liuxiwen: 'http://10.232.33.9:8082',
-    localhost: 'http://127.0.0.1:3001',
-    linlin: 'http://10.232.33.23:8082',
-    mqsas: 'http://sys.pt.miui.com',
-    test: 'http://test.sys.pt.miui.com',
-    preview: 'http://preview.sys.pt.miui.com',
-    zhicai: 'http://10.232.32.30:8082',
-    anqi: 'http://10.232.39.18:8082',
-    dawei: 'http://10.232.33.44:8088',
-    liuyilan: 'http://10.232.33.8:8082',
-    wangbin: 'http://10.232.33.42:8082',
-    maqihao: 'http://10.232.39.16:8082',
-    conf:'http://conf.sys.pt.miui.com'
-}
+
 const ips = [
     {
         name: '本地',
