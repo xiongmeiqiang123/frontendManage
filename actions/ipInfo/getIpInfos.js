d12c55719a13b0804d855274efb724b2b2b8b00d
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2018-02-27 15:17:32 +0800

    update

--- actions/ipInfo/getIpInfos.js
+++ actions/ipInfo/getIpInfos.js
@@ -2,5 +2,27 @@ const IpInfo = require('../../db/models/ipInfo.js')
 
 module.exports = async function (req, res, next) {
     let infos = await IpInfo.find({});
-    res.send({status: true, data: infos})
+    const data = infos.map((item) => {
+        const {
+            _id: id,
+            name,
+            ip,
+            port,
+            key,
+            msg,
+            isFront
+        } = item;
+        return {
+            id,
+            name,
+            ip,
+            port,
+            key,
+            msg,
+            isFront
+        }
+    })
+
+    // console.log(data);
+    res.send({status: true, data: data})
 };
