4f96bc45ebc8db12beb1ce33e4ad7818fc9cb7dc
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2018-04-08 10:27:19 +0800

    update get pvs

--- actions/pv/index.js
+++ actions/pv/index.js
@@ -1,21 +1,48 @@
-const Pv = require('../../db/models/Pv.js')
-const {getParamsFromReq} = require('../../components/util/index.js')
-const response = require('../../components/util/response')
-const _ = require('lodash')
-module.exports = async function (req, res, next) {
-    const {url, name} = getParamsFromReq(req)
-    Pv.find({}, {__v:false}).then(data => {
-        data = data.reduce((result, next) => {
-            let url = next.url;
-            if(result[url]) {
-                result[url] = result[url] + next.count
-            }else {
-                result[url] = next.count;
-            }
-            return result;
-        }, {})
-        res.send(response.success(data))
-    }).catch((err) => {
-        res.send(response.fail(err))
-    })
-}
+const Pv = require("../../db/models/Pv.js");
+const { getParamsFromReq } = require("../../components/util/index.js");
+const response = require("../../components/util/response");
+const _ = require("lodash");
+module.exports = async function(req, res, next) {
+    const { url, name, type  } = getParamsFromReq(req);
+
+    let data = await Pv.find({}, { __v: false }).catch(err => {
+        return [];
+    });
+    console.log(type, 'type');
+    switch (type) {
+        case '0':
+            data = data.reduce((result, next) => {
+                let url = next.url;
+                if (result[url]) {
+                    result[url] = result[url] + next.count;
+                } else {
+                    result[url] = next.count;
+                }
+                return result;
+            }, {});
+            data = Object.keys(data)
+                .map((url) => ({url: url, count: data[url]}))
+                .sort((a,b)=>(b.count - a.count))
+
+            break;
+        case '1':
+            data = data.reduce((result, next) => {
+                let url = next.url;
+                result[url] = result[url] || [];
+                let item = result[url]
+
+                if(item.indexOf(next.name) === -1) {
+                    item.push(next.name)
+                }
+                return result;
+            }, {});
+
+            data = Object.keys(data)
+                .map((url) => ({url: url, name: data[url]}))
+                .sort((a,b)=>(b.name.length - a.name.length))
+            break;
+        default:
+            break;
+    }
+    res.send(response.success(data));
+};
